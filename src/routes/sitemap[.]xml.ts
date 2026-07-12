1b3ba8119b9df034c4ec06bf9684569d1af5bc50
mates-1111 <matesprace11@gmail.com>
2026-07-12 09:34:02 +0200

    Update BASE_URL in sitemap.xml.ts

--- src/routes/sitemap[.]xml.ts
+++ src/routes/sitemap[.]xml.ts
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import type {} from "@tanstack/react-start";
 
-const BASE_URL = "https://thunder-chaser-hub.lovable.app";
+const BASE_URL = "https://bourkar-cz.vercel.app";
 
 interface SitemapEntry {
   path: string;
